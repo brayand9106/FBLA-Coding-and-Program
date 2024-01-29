3f0aaf2d6faa3945c4a0148944bb5c5fe773653f
brayand9106 <150633784+brayand9106@users.noreply.github.com>
2024-01-29 14:59:35 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -2,8 +2,20 @@ import PySimpleGUI as sg
 
 sg.theme("DarkGrey4") #The theme of the window for the program
 
+#Menu dropdown from filter button
+menu_def = ["filter", ['Alphabetical', 'Type of Organization']]
+
+buttonmenu = sg.ButtonMenu("Filter", menu_def, size=(50,50))
+
 #Layout of how the window looks
-layout = []
+layout = [[sg.Text("Industry Partners List", size=(100,100), justification="center")]]
 
 #Initializing the Window
-window = sg.Window("Industry Partners", size=(800, 400))#Window name, size, and layout
+window = sg.Window("Industry Partners", layout, size=(800, 400))#Window name, size, and layout
+
+#Loop of executing the window
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
+window.close()
