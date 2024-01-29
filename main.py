01546bc7624080fc1bc46aaa8232c7b64f548690
brayand9106 <150633784+brayand9106@users.noreply.github.com>
2024-01-29 12:57:47 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ sg.theme("DarkGrey4") #The theme of the window for the program
 
 #Layout of how the window looks
 layout = []
+
+#Initializing the Window
+window = sg.Window("Industry Partners", size=(800, 400))#Window name, size, and layout
