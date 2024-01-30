c740a51b3c6f688b91e71d07408f8fb6dd450e8e
brayand9106 <150633784+brayand9106@users.noreply.github.com>
2024-01-30 14:57:38 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import PySimpleGUI as sg
 
 sg.theme("DarkGrey4") #The theme of the window for the program
-sg.set_options(font=("Arial Bold", 20))
 #Menu dropdown from filter button
-menu_def = [['Filter', ['Alphabetical', 'Type of Organization']]]
+menu_def = [['Filter', ['Alphabetical', 'Type of Organization', 'Date']]]
 
+#Button File menu on topleft to make filter button for alphabetical
 buttonmenu = sg.ButtonMenu("Filter", menu_def, size=(50,50))
 
+
+######## TABLE VALUES AND TABLE LAYOUT ##########
+partnerCategories = ['Organization', 'Type of Organization', 'Contacts']
+partners = [['ASU Ira A. Fulton Schools of Engineering', 'Engineering School', 'FultonSchools@asu.edu'],
+            ['Intel Corporation', 'Semi-Conductor Chip Maker', 'intel.partner.marketing.studio@intel.com']
+
+]
+partnerTable = sg.Table(values=partners, headings=partnerCategories, font=('Arial', 10), justification= 'center', auto_size_columns=False, max_col_width=50, def_col_width=30, expand_x=True)
+
+#############
+
+
 #Layout of how the window looks
-layout = [[sg.Text("Industry Partners List", size=(40, 1), justification="center", expand_x=True)],
-          [sg.Menu(menu_def)]
+layout = [[sg.Text("Industry Partners List", size=(40, 1), justification="center", expand_x=True, font=("Arial Bold", 20))],
+          [sg.Menu(menu_def)], [partnerTable]
           ]
 
 #Initializing the Window
-window = sg.Window("Industry Partners", layout)#Window name, size, and layout
+window = sg.Window("Industry Partners", layout, size=(1000,420))#Window name, size, and layout
 
 #Loop of executing the window
 while True:
