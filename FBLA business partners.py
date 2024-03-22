e0a836bc3e617cf3438eb543c4d38eaf78cacff5
CristianF07 <150957336+CristianF07@users.noreply.github.com>
2024-03-22 11:37:40 -0700

    Partners
    One more description added.

--- FBLA business partners.py
+++ FBLA business partners.py
@@ -6,24 +6,23 @@ partners = [['Advance Micro Devices', 'Semi-Conductor Chip Makers', 'info@amd.co
             ['FICO', 'Business and Finance', 'scoresupport@fico.com']
             ['Hyatt Hotels', 'Business', 'consumeraffairs@hyatt.com'], 
             ['Juno', 'Business and Finance', 'hello@joinjuno.com'], 
-            ['IMA', 'Business and Finance', 'ima@imanet.org'], 
+            ['Institute of Management Accountants', 'Business and Finance', 'ima@imanet.org'], 
             ['RUBIN', 'Business and Finance', 'chelsea@rubineducation.com'], 
             ['Knowledge Matters', 'Education', 'VBCCentral@KnowledgeMatters.com'],
-
+######################################## The last 11 partners ##############################################################################################
             ['Eagle University', 'Education', 'taylor@eagleuniversity.org '],
             ['iD Tech', 'Education', 'hello@iDTech.com'],
-            ['NTHS', 'Education', 'info@nths.org'],
+            ['National Technical Honor Society', 'Education', 'info@nths.org'],
             ['Beta Camp', 'Education', 'hello@beta.camp'],
             ['CareerSafe', 'Business', 'support@careersafeonline.com'],
             ['City Pop', 'Community', 'taylor@citypopdenver.com'],
             ['fund2orgs', 'Community', 'asap@funds2orgs.com'],
             ['March of Dimes', 'Community', 'servicedesk@marchofdimes.org'],
-            ['NSHSS', 'Education', 'information@nshss.org'],
             ['SCAD', 'Education', 'techsupport@scad.edu'],
-            ['Meta', 'Education', 'info@meta.edu.np']] 
-
-
+            ['Meta', 'Education', 'info@meta.edu.np'],
+            ['Accountant of International Certified Professional Accountants', 'Education', 'startheregoplaces@aicpa.org']] 
+######################################## Some of the descriptions #########################################################################################
 ['Meta', 'Providing the basics of digital marketing to help aid educators with online content such as online quizzes, and lessons through full flexibility']
 ['SCAD', 'Bringing students from more than 100 countries with over 100 academic degree programs. Internships, certifications, and collaborative projects are all involved in the SCAD curriculum']
-['NSHSS', '']
+['Accountant of International Certified Professional Accountants', 'Learn how to become a certified professional accountant with a fully personalized College Checklist, as well as the many opportunities to learn from professionals']
             
