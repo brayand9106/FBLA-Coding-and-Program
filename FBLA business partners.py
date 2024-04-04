40eb3af98522e23d1492065b836bef6ba693d679
CristianF07 <150957336+CristianF07@users.noreply.github.com>
2024-04-04 11:32:45 -0700

    Add files via upload

--- FBLA business partners.py
+++ FBLA business partners.py
@@ -20,9 +20,27 @@ partners = [['Advance Micro Devices', 'Semi-Conductor Chip Makers', 'info@amd.co
             ['March of Dimes', 'Community', 'servicedesk@marchofdimes.org'],
             ['SCAD', 'Education', 'techsupport@scad.edu'],
             ['Meta', 'Education', 'info@meta.edu.np'],
-            ['Accountant of International Certified Professional Accountants', 'Education', 'startheregoplaces@aicpa.org']] 
+            ['AICPA', 'Education', 'startheregoplaces@aicpa.org']] 
 ######################################## Some of the descriptions #########################################################################################
+['AICPA', 'Learn how to become a certified professional accountant with a fully personalized College Checklist, as well as the many opportunities to learn from professionals.']
 ['Meta', 'Providing the basics of digital marketing to help aid educators with online content such as online quizzes, and lessons through full flexibility']
-['SCAD', 'Bringing students from more than 100 countries with over 100 academic degree programs. Internships, certifications, and collaborative projects are all involved in the SCAD curriculum']
-['Accountant of International Certified Professional Accountants', 'Learn how to become a certified professional accountant with a fully personalized College Checklist, as well as the many opportunities to learn from professionals']
-            
+['SCAD', 'Bringing students from more than 100 countries with over 100 academic degree programs. Internships, certifications, and collaborative projects are all involved in the SCAD curriculum.']
+['March of Dimes', 'Help aid mothers and babies through the advocation of health by raising money to assist soon to be mothers, and mothers in need.']
+['fund2orgs', 'Create fundraising goals for all FBLA chapters with tools provided for by fund2org, helping not only FBLA, but those in need.']
+['City Pop', 'A new initiative way to do fundraisers, providing FBLA chapters with product lines that gives half of the money received back with no hassles or difficulties.']
+['CareerSafe', 'Training more than 1.2 million students, CarrerSafe focuses on providing students with a safe future by giving them proper training for online safety.']
+['Beta Camp', 'A virtual program for high school students to make more future entrepreneurs and leaders, featuring a fully interactive online workspace to ensure the proper training of students.']
+['Nation Technical Honors Society', 'Acknowledging the hard work of CTE students, and FBLA memebers alike, NTHS encourages students to perform at their best, and eventually work in a skilled workspace with the many scholarships given.']
+['iD Tech', 'Preparing students for college by enhancing various skills, iD Tech has everything covered ranging from private lessons, to Summer camps to ensure the best outcome for college admissions.']
+['Eagle University', 'Giving students a 7 year headstart on their career, Eagle University is open to students 15-25 years old with career planning, performmance strategy, and much more.']
+['Knowledge Matters', 'Compete in a competitive event that test the skills needed for future finances such as opening bank accounts, saving, and budgeting']
+['RUBIN', 'Teaching students how to how to improve employability skills by teaching proper ways to write emails.']
+['Institue of Management Accountants', 'IMA offers finance and business students with the necesary information for success through conferences, scholarships, competitions and much more.']
+['Juno', 'Starting from Harvard Business School, Juno is made to alleviate the pressure put on students by student loans by negotiating savings for free.']
+['Hyatt Hotels', 'Providing FBLA with hotels for various events and conferences for competitions and more.']
+['FICO', 'Educating students on the importance of credit literacy through free education in fun and simple ways for the most effective learning experience.']
+['Equidi', 'A platform to showcase the many skills that students have that allows for the connection between other students in a professional manner.']
+['BusinessU', 'Preparing Students to become proficcient in the business field with courses including Marketing, Accounting, Entrepreneurship, and much more.']
+['Geico', 'Providing FBLA members with car insurance at a special discount.']
+['Amazon', 'Every item purchased on Amazon supports FBLA.']
+['Advanced Micro Devices', 'Microchip company that develops a variety of computer parts for businesses and consumer markets.']
